61761df8ba5cbe27564d0ca1ee9954e154ccb2c6
Ashok-Maruvada <ashokmaruvada6174@gmail.com>
2024-04-23 17:04:58 +0530

    project

---
+++ backend.sh
@@ -0,0 +1,82 @@
+#!/bin/bash
+
+userid=$(id -u)
+timestamp=$(date +%F-%H-%M-%S)
+script_name=$(echo $0 | cut -d "." -f1)
+LOGFILE=/tmp/$script_name-$timestamp.log
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+echo " please enter DB password"
+read -s mysql_root_password
+validate(){
+    if [ $1 -ne 0 ]
+    then 
+        echo -e "$2 : $R failed $N "
+        exit 1
+    else
+        echo -e "$2 : $G sucees $N "
+    fi
+}
+if [ $userid -ne 0 ]
+then
+    echo "please use root access"
+    exit 1
+else
+    echo " u are super user"
+fi
+
+dnf module disable nodejs -y &>>$LOGFILE
+validate $? "disabling default nodejs"
+
+dnf module enable nodejs:20 -y &>>$LOGFILE
+validate $? "enabling nodejs:20 version "
+
+dnf install nodejs -y &>>$LOGFILE
+validate $? "installing nodejs"
+
+id expense &>>$LOGFILE
+if [ $? -eq 0 ]
+then
+    echo -e " user expense is already exist: $Y skipping $N "
+    exit 1
+else 
+    useradd expense
+    validate $? " creating user: expense"
+fi
+
+mkdir -p /app
+validate $? "creating app directory"
+
+curl -o /tmp/backend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-backend-v2.zip
+validate $? "downloading backend code"
+
+cd /app
+rm -rf /app/*
+unzip /tmp/backend.zip
+validate $? "unzipping backend code to app dir"
+
+npm install
+validate $? "installing nodejs dependencies"
+
+cp <backend.service-path> /etc/systemd/system/backend.service
+validate $? "copied backend service"
+
+systemctl daemon-reload &>>$LOGFILE
+validate $? "reloading daemon service"
+
+systemctl start backend &>>$LOGFILE
+validate $? "starting backend"
+
+systemctl enable backend &>>$LOGFILE
+validate $? "enabling backend"
+
+dnf install mysql -y &>>$LOGFILE
+validate $? "installing mysql client"
+
+mysql -h <mysql-ip> -uroot -p${mysql_root_password} < /app/schema/backend.sql &>>$LOGFILE
+validate $? "loading schema"
+
+systemctl restart backend &>>$LOGFILE
+validate $? "restarting the backend"
